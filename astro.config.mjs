fc2b07a12f4bf15e66dbdd12540a19d1f0e0166b
wetarorg <tarframework@gmail.com>
2024-01-07 20:41:28 +0530

    1.012

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import vue from "@astrojs/vue";
+
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), vue()],
   output: 'server',
+  adapter: cloudflare()
 });
